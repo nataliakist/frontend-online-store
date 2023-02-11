3aa34ed5aa182c375126f98ee90f5009601d250e
andre <andrespeltzalbernaz@gmail.com>
2023-02-10 22:04:50 -0300

    requititos 6 e 7 prontos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Search from './components/Search';
 import ShoppingCart from './components/ShoppingCart';
+import Products from './components/Products';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Search } />
         <Route path="/cart" component={ ShoppingCart } />
+        <Route exact path="/products/:id" component={ Products } />
       </Switch>
     </div>
 
